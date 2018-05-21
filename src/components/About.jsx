042956fe6d4037861c06ff0097cf3927e6de34cb
princessguerrero <princessguerrero@ac.c4q.nyc>
2018-05-21 12:25:14 -0400

    edit about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,14 +19,14 @@ class About extends Component {
             <img src={Pjimg2} className="photo" />
           </div>
           <div className="centeredAbout">
-            I am a web developer living in New York City. <br />
-            Before diving into web development,  I was a stay-at-home
-            mom. In trying to connect with my child I learned that he was not
-            reaching his milestones. He had challenges other kids did not have.
-            Because of that I became passionate about childhood development. I
-            realized that if I learn how to code, not only will I have a career
-            in tech but also be able to build products that are helpful for
-            individuals in the Autism Spectrum and their caregivers.
+            I am a web developer living in New York City and before diving into
+            web development, I was a stay-at-home mom. In trying to connect with
+            my child I learned that he was not reaching his milestones. He had
+            challenges other kids did not have. Because of that I became
+            passionate about childhood development and I realized that if I
+            learn how to code, not only will I have a career in tech but also be
+            able to build products that are helpful for individuals in the
+            Autism Spectrum and their caregivers.
           </div>
         </div>
       </div>

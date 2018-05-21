8a9c811ce6620da003cd2409543856cf7422996a
princessguerrero <princessguerrero@ac.c4q.nyc>
2018-05-21 12:05:39 -0400

    fix formatting

--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -8,19 +8,32 @@ class GetInTouch extends Component {
         <div className="getInTouchTitle">Get In Touch</div>
         <div className="container">
           <div className="icon">
-            <a href="https://drive.google.com/file/d/1322cTsGYjq9czT5b-PcrbKialxiSxKYX/view?usp=sharing"><i class="far fa-3x fa-file-alt" /></a>
+            <a href="https://drive.google.com/file/d/1322cTsGYjq9czT5b-PcrbKialxiSxKYX/view?usp=sharing">
+              <i class="far fa-3x fa-file-alt" />
+            </a>
           </div>
           <div className="icon">
-            <a href="https://www.linkedin.com/in/princess-guerrero/" target="_blank"><i class="fab fa-3x fa-linkedin-in" /></a>
+            <a
+              href="https://www.linkedin.com/in/princess-guerrero/"
+              target="_blank"
+            >
+              <i class="fab fa-3x fa-linkedin-in" />
+            </a>
           </div>
           <div className="icon">
-            <a href="https://twitter.com/princessg814" target="_blank"><i class="fab fa-3x fa-twitter" /></a>
+            <a href="https://twitter.com/princessg814" target="_blank">
+              <i class="fab fa-3x fa-twitter" />
+            </a>
           </div>
           <div className="icon">
-            <a href="https://github.com/princessguerrero" target="_blank"><i class="fab fa-3x fa-github" /></a>
+            <a href="https://github.com/princessguerrero" target="_blank">
+              <i class="fab fa-3x fa-github" />
+            </a>
           </div>
           <div className="icon">
-            <a href="mailto:princessguerrero@ac.c4q.nyc"><i class="far fa-3x fa-envelope" /></a>{" "}
+            <a href="mailto:princessguerrero@ac.c4q.nyc">
+              <i class="far fa-3x fa-envelope" />
+            </a>{" "}
           </div>
         </div>
       </div>
